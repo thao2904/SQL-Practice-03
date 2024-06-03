7a39467d61f31ea706f14ea68fc756ee7c0f8cce
thao2904 <nguyenth@dickinson.edu>
2024-06-03 07:05:42 -0400

    Update Practice03.sql

--- Practice03.sql
+++ Practice03.sql
@@ -1,1 +1,11 @@
+--Practice01:
+SELECT Name from STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3 ), ID
+--Practice02:
+SELECT user_id,
+CONCAT(UPPER(LEFT(name,1)),LOWER(RIGHT(name, length(name)-1))) as name
+FROM Users
+ORDER BY user_id;
+--Practice03:
 
